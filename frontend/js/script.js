154947c1f3da12455152548694b51129f022c8fb
Diego Paiva Machado <tqi_dmachado@uolinc.com>
2023-05-23 14:26:35 -0300

    Adicionando Menu css magic no index.html

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -164,3 +164,29 @@ const loadTasks = async () => {
 
 addForm.addEventListener("submit", addTask);
 loadTasks();
+
+/**
+ * Funções responsável pelo MENU
+ */
+const indicator = document.querySelector("[data-indicator]");
+document.addEventListener("click", (e) => {
+    let anchor;
+    if (e.target.matches("a")) {
+        anchor = e.target;
+    } else {
+        anchor = e.target.closest("a");
+    }
+    if (anchor != null) {
+        const allAnchors = [...document.querySelectorAll("a")];
+        const index = allAnchors.indexOf(anchor);
+        indicator.style.setProperty("--position", index);
+        document.querySelectorAll("a").forEach((elem) => {
+            elem.classList.remove("active");
+        });
+        anchor.classList.add("active");
+
+        // Redirecionamento
+        const url = anchor.getAttribute("href");
+        window.location.href = url;
+    }
+});
